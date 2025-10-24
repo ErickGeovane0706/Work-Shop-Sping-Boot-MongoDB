6d1d9e525ec45a54cf19c5a08a98543b7087bf4a
Erick <erickgeovane2002@gmail.com>
2025-10-24 14:20:25 -0300

    Dependencia MongoDB, Mapeamento User e conexão com banco de dados

--- src/main/java/org/projetos/workshopmongo/resources/UserResource.java
+++ src/main/java/org/projetos/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package org.projetos.workshopmongo.resources;
 
 import org.projetos.workshopmongo.domain.User;
+import org.projetos.workshopmongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,12 +15,11 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1","Maria Silva","maria@gmail.com");
-        User alex = new User("2", "Alex","Alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/org/projetos/workshopmongo/service/UserService.java
@@ -0,0 +1,19 @@
+package org.projetos.workshopmongo.service;
+
+import org.projetos.workshopmongo.domain.User;
+
+import org.projetos.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
