94fbbfeb2213988651350237c5586c0786f8f145
Erick <erickgeovane2002@gmail.com>
2025-10-26 12:11:21 -0300

    Criação do Insert

--- src/main/java/org/projetos/workshopmongo/service/UserService.java
+++ src/main/java/org/projetos/workshopmongo/service/UserService.java
@@ -2,6 +2,7 @@ package org.projetos.workshopmongo.service;
 
 import org.projetos.workshopmongo.domain.User;
 
+import org.projetos.workshopmongo.dto.UserDTO;
 import org.projetos.workshopmongo.repository.UserRepository;
 import org.projetos.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,11 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Usuário " + id + " não encontrado"));
+    }
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
     }
 }
